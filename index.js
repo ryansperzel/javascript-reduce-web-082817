76b7754d462979c54ec42e241cff4fa954e3500e
Ryan Sperzel <ryansperzel@gmail.com>
2017-10-27 10:38:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much."
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue;
+
+  collection.forEach((product, index) => {
+    result = callback(result, product, index, collection);
+  });
+
+  return result;
+}
